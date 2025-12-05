9280d1c0175bad23a163e7c704bf1253ba2796eb
Prajwal <atharep1703@gmail.com>
2025-12-05 22:54:26 +0530

    Update search.ts

--- routes/search.ts
+++ routes/search.ts
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
 
 import { type Request, type Response, type NextFunction } from 'express'
 
